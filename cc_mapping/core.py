3f6d9d47e08b63ee25af1da32c47f11ab6ad85b5
ddpoe <dap182@pitt.edu>
2023-11-28 14:58:28 -0500

    added classification report to RF function

--- cc_mapping/core.py
+++ cc_mapping/core.py
@@ -24,6 +24,7 @@ def random_forest_feature_selection(adata: ad.AnnData,
                                     training_labels: str,
                                     feature_set_name: str = None,
                                     method: str = 'RF_min_max',
+                                    stable_counter: int = 10,
                                     ):
 
     feature_set_idxs, _ = get_str_idx(training_feature_set, adata.var_names.values)
@@ -50,7 +51,7 @@ def random_forest_feature_selection(adata: ad.AnnData,
     features = trunc_cell_data
 
     # Split the data into training and testing sets
-    train_features, _, train_labels, _ = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
 
     # Instantiate model 
     rf_classifier = RandomForestClassifier(
@@ -62,6 +63,12 @@ def random_forest_feature_selection(adata: ad.AnnData,
                         random_state=42)
 
     rf_classifier.fit(train_features, train_labels)
+
+    rf_pred_labels=rf_classifier.predict(test_features)
+    accuracy = metrics.accuracy_score(test_labels, rf_pred_labels)
+    print(metrics.classification_report(test_labels, rf_pred_labels))
+
+    print(f'Primary Random Forest Accuracy: {accuracy}')
         
     #negative to have it sort from highest to lowest
     sorted_idxs = np.argsort(-rf_classifier.feature_importances_)
@@ -72,7 +79,7 @@ def random_forest_feature_selection(adata: ad.AnnData,
         optim_RF_feature_set = sorted_feature_set[:30]
     elif method == 'RF_min_max':
 
-        stable_counter = 0
+        counter = 0
         max_acc_arg = 0
         acc_list = []
         for num_cols in range(1, sorted_features.shape[1]+1):
@@ -97,20 +104,31 @@ def random_forest_feature_selection(adata: ad.AnnData,
             acc_list.append([num_cols,accuracy])
 
             if max_acc_arg == np.argmax(np.array(acc_list)[:,1]):
-                stable_counter += 1
+                best_model = True
+                counter += 1
             else:
+                best_model = False
                 max_acc_arg = np.argmax(np.array(acc_list)[:,1])
-                stable_counter = 0
+                counter = 0
 
-            if stable_counter == 10:
+            if not best_model:
+                best_rf_classifier = trunc_rf_classifier
+
+            if counter == stable_counter:
                 break
 
+        trunc_rf_classifier.fit(trunc_train_features, trunc_train_labels)
+
+        trunc_pred_labels=trunc_rf_classifier.predict(trunc_test_features)
+
+        print(metrics.classification_report(trunc_test_labels, trunc_pred_labels))
 
         acc_list = np.array(acc_list)
         max_accuracy = acc_list[max_acc_arg,1]
         optim_feat_num = max_acc_arg + 1
         optim_RF_feature_set = sorted_feature_set[:optim_feat_num]
 
+    print(f'Max Secondary Random Forest Accuracy: {max_accuracy}')
 
     feat_idxs, _ = get_str_idx(optim_RF_feature_set, adata.var_names.values)
 
